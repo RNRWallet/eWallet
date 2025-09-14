efc493a091b7f9e9fadc119008a68f7d9b8ced55
V K <koneru.chowdary@gmail.com>
2025-09-14 14:00:24 +0200

    Fix base url docusaurus.config.ts

--- docs/e-hailing/docusaurus.config.ts
+++ docs/e-hailing/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: "https://rnrwallet.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/eWallet/docs/e-hailing/",
+  baseUrl: "/eWallet/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
